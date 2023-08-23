d81d83f9ca380fb0ace17c8e8338a8fd5a7e153a
michauwu <46261510+michauwu@users.noreply.github.com>
2023-08-23 20:11:52 +0200

    Update python_tricks.py

--- python_tricks.py
+++ python_tricks.py
@@ -3,3 +3,7 @@
 my_list = [1,2,3,4,4,5,2,3,4]
 print(set(my_list))
 #output {1, 2, 3, 4, 5}
+
+#function to convert string with camel case to separate by space camelCase -> camel Case
+def solution(s):
+    return ''.join(' ' + c if c.isupper() else c for c in s)
